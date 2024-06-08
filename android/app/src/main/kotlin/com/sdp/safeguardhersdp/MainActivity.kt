67700f8851984abd874e6b61311ff52ae0a1d501
MayeeshaMusarrat <musarratmayeesha@gmail.com>
2024-06-08 22:08:33 +0600

    feat: home page

--- android/app/src/main/kotlin/com/sdp/safeguardhersdp/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.sdp.safeguardhersdp
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,12 +8,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:safeguardhersdp/main.dart';
+import 'package:sdp/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const SafeGuardHerApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
